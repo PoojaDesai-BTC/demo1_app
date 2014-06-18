c34a3b6fb0cf3a28bbcc06d68ff91c6c1f980433
Pooja <pooja.desai@beaverlogic.com>
2014-06-18 13:02:59 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo1App::Application.config.secret_key_base = '9c430fc910d0120669802a299bb47fc0a3e66fb1e34eb2730147284d8e07f7ed7caf3b79acdc88a7662a4bc7e78c6759cf4f1f9467c4c6918d295aeef82b8600'
